7c3752e77b70a33b4a39783107323094f78a8ec8
dev-dimas <75769207+dev-dimas@users.noreply.github.com>
2023-03-01 13:57:14 +0700

    Add answer

---
+++ index.js
@@ -0,0 +1,314 @@
+// 1. Buatlah 20 variable yang dipakai untuk menghitung 5 angka, outputkan hasilnya
+
+let firstVariable = 2 + 3 * 4 - 1 / 5;
+console.log(firstVariable);
+
+let secondVariable = (8 % 3) * 5 - 2 + 1;
+console.log(secondVariable);
+
+let thirdVariable = 9 + 7 / 2 - 1 * 3;
+console.log(thirdVariable);
+
+let fourthVariable = (6 * 5) / 2 + 1 - 3;
+console.log(fourthVariable);
+
+let fifthVariable = 10 - 2 ** 2 * 3 + 5;
+console.log(fifthVariable);
+
+let sixthVariable = 1 + 3 * 5 - 8 / 4;
+console.log(sixthVariable);
+
+let seventhVariable = (7 % 3) + 2 * 4 - 1;
+console.log(seventhVariable);
+
+let eighthVariable = (6 / 2) * 4 - 3 + 1;
+console.log(eighthVariable);
+
+let ninthVariable = 5 ** 2 / 5 - 1 * 4;
+console.log(ninthVariable);
+
+let tenthVariable = 8 - 2 + (7 / 2) * 3;
+console.log(tenthVariable);
+
+let eleventhVariable = 2 ** 4 - 3 * 2 + 4;
+console.log(eleventhVariable);
+
+let twelfthVariable = 7 / 2 - 1 * 4 + 5;
+console.log(twelfthVariable);
+
+let thirteenthVariable = (5 * 2) / 5 + 1 * 4;
+console.log(thirteenthVariable);
+
+let fourteenthVariable = 9 - 5 + (3 / 3) * 2;
+console.log(fourteenthVariable);
+
+let fifteenthVariable = (7 % 2) * 5 + 3 + 2;
+console.log(fifteenthVariable);
+
+let sixteenthVariable = 3 * 4 - 2 + 8 / 4;
+console.log(sixteenthVariable);
+
+let seventeenthVariable = 6 / 3 + 2 ** 2 + 1;
+console.log(seventeenthVariable);
+
+let eighteenthVariable = 1 + (2 * 3) / 6 - 5;
+console.log(eighteenthVariable);
+
+let nineteenthVariable = (5 % 2) * 3 + 4 * 2;
+console.log(nineteenthVariable);
+
+let twentiethVariable = 8 - 2 + 5 ** 2 / 5;
+console.log(twentiethVariable);
+
+/* ============================================================
+============================================================
+============================================================ */
+
+// 2. Buat masing masing 10 dari setiap operator perbandingan, outputkan hasilnya
+// - Operator < (Less Than)
+let firstVariableLt = 2 < 3;
+console.log(firstVariableLt);
+
+let secondVariableLt = 4.5 < 4.7;
+console.log(secondVariableLt);
+
+let thirdVariableLt = -1 < 0;
+console.log(thirdVariableLt);
+
+let fourthVariableLt = 8 < 8;
+console.log(fourthVariableLt);
+
+let fifthVariableLt = 0.2 < 0.02;
+console.log(fifthVariableLt);
+
+let sixthVariableLt = 10 < 2;
+console.log(sixthVariableLt);
+
+let seventhVariableLt = -5 < -10;
+console.log(seventhVariableLt);
+
+let eighthVariableLt = 100 < 50;
+console.log(eighthVariableLt);
+
+let ninthVariableLt = 3.14 < 2;
+console.log(ninthVariableLt);
+
+let tenthVariableLt = -2 < -2;
+console.log(tenthVariableLt);
+
+// - Operator > (Greater Than)
+let firstVariableGt = 7 > 2;
+console.log(firstVariableGt);
+
+let secondVariableGt = 3.5 > 1.2;
+console.log(secondVariableGt);
+
+let thirdVariableGt = -2 > -5;
+console.log(thirdVariableGt);
+
+let fourthVariableGt = 100 > 100;
+console.log(fourthVariableGt);
+
+let fifthVariableGt = 0.1 > 0.5;
+console.log(fifthVariableGt);
+
+let sixthVariableGt = -10 > -5;
+console.log(sixthVariableGt);
+
+let seventhVariableGt = 2 > 10;
+console.log(seventhVariableGt);
+
+let eighthVariableGt = 3 > 3.0;
+console.log(eighthVariableGt);
+
+let ninthVariableGt = 1 > -1;
+console.log(ninthVariableGt);
+
+let tenthVariableGt = 999 > 1000;
+console.log(tenthVariableGt);
+
+// - Operator <= (Less Than Or Equal)
+let firstVariableLte = 2 <= 4;
+console.log(firstVariableLte);
+
+let secondVariableLte = 6 <= 6;
+console.log(secondVariableLte);
+
+let thirdVariableLte = 0 <= -1;
+console.log(thirdVariableLte);
+
+let fourthVariableLte = 4.5 <= 4.6;
+console.log(fourthVariableLte);
+
+let fifthVariableLte = -5 <= -5;
+console.log(fifthVariableLte);
+
+let sixthVariableLte = 10 <= 2;
+console.log(sixthVariableLte);
+
+let seventhVariableLte = 3 <= 2;
+console.log(seventhVariableLte);
+
+let eighthVariableLte = 100 <= 50;
+console.log(eighthVariableLte);
+
+let ninthVariableLte = 3.14 <= 3.14;
+console.log(ninthVariableLte);
+
+let tenthVariableLte = -2 <= -1;
+console.log(tenthVariableLte);
+
+// - Operator >= (Greater Than Or Equal)
+let firstVariableGte = 8 >= 2;
+console.log(firstVariableGte);
+
+let secondVariableGte = 5 >= 5;
+console.log(secondVariableGte);
+
+let thirdVariableGte = -3 >= -5;
+console.log(thirdVariableGte);
+
+let fourthVariableGte = 2.5 >= 3.14;
+console.log(fourthVariableGte);
+
+let fifthVariableGte = 7 >= 10;
+console.log(fifthVariableGte);
+
+let sixthVariableGte = 0 >= -1;
+console.log(sixthVariableGte);
+
+let seventhVariableGte = 10.2 >= 10.2;
+console.log(seventhVariableGte);
+
+let eighthVariableGte = -10 >= -9;
+console.log(eighthVariableGte);
+
+let ninthVariableGte = 100 >= 99.99;
+console.log(ninthVariableGte);
+
+let tenthVariableGte = 2 >= 2;
+console.log(tenthVariableGte);
+
+// - Operator == (Equal)
+let firstVariableEq = 2 == '2';
+console.log(firstVariableEq);
+
+let secondVariableEq = 5 == 2;
+console.log(secondVariableEq);
+
+let thirdVariableEq = 0 == 2;
+console.log(thirdVariableEq);
+
+let fourthVariableEq = 9 == '2';
+console.log(fourthVariableEq);
+
+let fifthVariableEq = 2 == 1;
+console.log(fifthVariableEq);
+
+let sixthVariableEq = 7 == 0;
+console.log(sixthVariableEq);
+
+let seventhVariableEq = 3 == 1;
+console.log(seventhVariableEq);
+
+let eighthVariableEq = 9 == 9.0;
+console.log(eighthVariableEq);
+
+let ninthVariableEq = -1 == 1;
+console.log(ninthVariableEq);
+
+let tenthVariableEq = 10 == 9.9;
+console.log(tenthVariableEq);
+
+// - Operator === (Equal Strict)
+let firstVariableEs = 2 === '2';
+console.log(firstVariableEs);
+
+let secondVariableEs = 5 === 5.0;
+console.log(secondVariableEs);
+
+let thirdVariableEs = 5 === 8;
+console.log(thirdVariableEs);
+
+let fourthVariableEs = 3 === 8;
+console.log(fourthVariableEs);
+
+let fifthVariableEs = 5 === 3;
+console.log(fifthVariableEs);
+
+let sixthVariableEs = 4 === 6;
+console.log(sixthVariableEs);
+
+let seventhVariableEs = 22 === 75;
+console.log(seventhVariableEs);
+
+let eighthVariableEs = 6 === '6';
+console.log(eighthVariableEs);
+
+let ninthVariableEs = 5 === 4;
+console.log(ninthVariableEs);
+
+let tenthVariableEs = 2 === 2;
+console.log(tenthVariableEs);
+
+// - Operator != (Not Equal)
+let firstVariableNe = 2 != 5;
+console.log(firstVariableNe);
+
+let secondVariableNe = 2 != '2';
+console.log(secondVariableNe);
+
+let thirdVariableNe = 9 != 22;
+console.log(thirdVariableNe);
+
+let fourthVariableNe = 9.0 != 9;
+console.log(fourthVariableNe);
+
+let fifthVariableNe = 5 != '5';
+console.log(fifthVariableNe);
+
+let sixthVariableNe = 100 != 10;
+console.log(sixthVariableNe);
+
+let seventhVariableNe = 12 != 24;
+console.log(seventhVariableNe);
+
+let eighthVariableNe = 24 != 48;
+console.log(eighthVariableNe);
+
+let ninthVariableNe = 2 != 2;
+console.log(ninthVariableNe);
+
+let tenthVariableNe = 2 != 4;
+console.log(tenthVariableNe);
+
+// - Operator !== (Not Equal Strict)
+let firstVariableNes = 2 !== 5;
+console.log(firstVariableNes);
+
+let secondVariableNes = 0 !== 0;
+console.log(secondVariableNes);
+
+let thirdVariableNes = -2 !== 2;
+console.log(thirdVariableNes);
+
+let fourthVariableNes = 4.8 !== 4.8;
+console.log(fourthVariableNes);
+
+let fifthVariableNes = -5.2 !== -5.2;
+console.log(fifthVariableNes);
+
+let sixthVariableNes = 20 !== 10;
+console.log(sixthVariableNes);
+
+let seventhVariableNes = 100 !== 200;
+console.log(seventhVariableNes);
+
+let eighthVariableNes = 1.5 !== 1;
+console.log(eighthVariableNes);
+
+let ninthVariableNes = -10 !== -11;
+console.log(ninthVariableNes);
+
+let tenthVariableNes = 3.14 !== 3.14;
+console.log(tenthVariableNes);
